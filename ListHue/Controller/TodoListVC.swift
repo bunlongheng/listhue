e5ebe47130cd036379cb85624083e62dee2e338e
Bunlong Heng <bunlongheng@outlook.com>
2018-08-01 14:10:06 -0400

    finishing up with single plist

--- ListHue/Controller/TodoListVC.swift
+++ ListHue/Controller/TodoListVC.swift
@@ -12,35 +12,19 @@ class TodoListVC: UITableViewController {
 
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
     
+    var dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
     
     // ---------------------------------------------------------------------------------------------------------
     //MARK - viewDidLoad
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(dataFilePath)
         
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        newItem.done = true
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        newItem2.done = false
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Kill Demogorgon"
-        newItem3.done = false
-        itemArray.append(newItem3)
-        
-        if let items = defaults.array(forKey: "ItemArray") as? [Item] {
-            itemArray = items
-        }
-        
+        loadItems()
     }
     
 
@@ -52,7 +36,6 @@ class TodoListVC: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("cellForRowAtIndexPath Called")
         let cell = tableView.dequeueReusableCell(withIdentifier: "listItemCell", for: indexPath)
         let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
@@ -65,6 +48,7 @@ class TodoListVC: UITableViewController {
     //MARK - TableView Delegate Method
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        self.saveItems()
         self.tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -81,31 +65,53 @@ class TodoListVC: UITableViewController {
         //action
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-        
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-            
-            self.defaults.set(self.itemArray, forKey: "ItemArray")
+            self.saveItems()
             self.tableView.reloadData()
             
         }
         
-
         alert.addTextField { (alertTextField) in
-            
             alertTextField.placeholder = "Create new item"
-            
-            print(alertTextField.text)
             textField = alertTextField
-            
         }
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
         
+    }
+    
+    // ---------------------------------------------------------------------------------------------------------
+    //MARK - Model Manipulation Methods
+    
+    
+    func saveItems() {
         
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+    }
+    
+    func loadItems() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            
+            do {
+               itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
     }
     
     
